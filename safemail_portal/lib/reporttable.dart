c6c1900e1ed65440c0a019ea3066bc03c42e7033
Andy Wang <chujian.wang@west.com>
2022-01-19 17:48:08 -0500

    Modify table.

--- safemail_portal/lib/reporttable.dart
+++ safemail_portal/lib/reporttable.dart
@@ -41,22 +41,94 @@ class _reporttableState extends State<reporttable> {
                 Text("REFERRALS SENT", textScaleFactor: 1),
               ]),
               TableRow(children: [
-                Text("B.Tech", textScaleFactor: 1),
-                Text("ABESEC", textScaleFactor: 1),
-                Text("AKTU", textScaleFactor: 1),
-                Text("AKTU", textScaleFactor: 1),
+                Text("01/04", textScaleFactor: 1),
+                Text("204", textScaleFactor: 1),
+                Text("280", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
               ]),
               TableRow(children: [
-                Text("12th", textScaleFactor: 1),
-                Text("Delhi Public School", textScaleFactor: 1),
-                Text("CBSE", textScaleFactor: 1),
-                Text("AKTU", textScaleFactor: 1),
+                Text("01/05", textScaleFactor: 1),
+                Text("199", textScaleFactor: 1),
+                Text("103", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
               ]),
               TableRow(children: [
-                Text("High School", textScaleFactor: 1),
-                Text("SFS", textScaleFactor: 1),
-                Text("ICSE", textScaleFactor: 1),
-                Text("AKTU", textScaleFactor: 1),
+                Text("01/06", textScaleFactor: 1),
+                Text("242", textScaleFactor: 1),
+                Text("197", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/07", textScaleFactor: 1),
+                Text("254", textScaleFactor: 1),
+                Text("141", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/08", textScaleFactor: 1),
+                Text("238", textScaleFactor: 1),
+                Text("43", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/09", textScaleFactor: 1),
+                Text("57", textScaleFactor: 1),
+                Text("13", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/10", textScaleFactor: 1),
+                Text("263", textScaleFactor: 1),
+                Text("100", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/11", textScaleFactor: 1),
+                Text("288", textScaleFactor: 1),
+                Text("61", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/12", textScaleFactor: 1),
+                Text("240", textScaleFactor: 1),
+                Text("75", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/13", textScaleFactor: 1),
+                Text("350", textScaleFactor: 1),
+                Text("109", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/14", textScaleFactor: 1),
+                Text("346", textScaleFactor: 1),
+                Text("175", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/15", textScaleFactor: 1),
+                Text("238", textScaleFactor: 1),
+                Text("122", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/16", textScaleFactor: 1),
+                Text("53", textScaleFactor: 1),
+                Text("4", textScaleFactor: 1),
+                Text("0", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/17", textScaleFactor: 1),
+                Text("66", textScaleFactor: 1),
+                Text("94", textScaleFactor: 1),
+                Text("1", textScaleFactor: 1),
+              ]),
+              TableRow(children: [
+                Text("01/18", textScaleFactor: 1),
+                Text("148", textScaleFactor: 1),
+                Text("51", textScaleFactor: 1),
+                Text("1", textScaleFactor: 1),
               ]),
             ],
           ),
@@ -67,5 +139,8 @@ class _reporttableState extends State<reporttable> {
 }
 
 class ReportItem{
-  final String date='';
+  late String date;
+  late int Filtered;
+  late int Reviewed;
+  late int Referals;
 }
